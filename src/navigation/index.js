49057b1c18881bea4dc44030d8b29bc35dc8c308
Oleg Dramaretskyi <dramarec@gmail.com>
2021-10-05 21:30:34 +0300

    add navigation screens

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,39 +1,131 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { Calendar, Dashboard, Medication } from '../screens';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
+import { TouchableOpacity } from 'react-native';
 
-export default function Navigation() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function DashboardScreenNavigator() {
     return (
-        <NavigationContainer>
-            <RootNavigator />
-        </NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 22,
+                },
+            }}>
+            <Stack.Screen
+                name="dashboard"
+                component={Dashboard}
+                options={() => {
+                    return {
+                        headerLeft: () => (
+                            <TouchableOpacity style={{ marginRight: 20 }}>
+                                <Feather
+                                    name="menu"
+                                    size={24}
+                                    backgroundColor="#ccfd"
+                                />
+                            </TouchableOpacity>
+                        ),
+                    };
+                }}
+            />
+        </Stack.Navigator>
     );
 }
 
-const Tab = createBottomTabNavigator();
+function MedicationScreenNavigator() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 22,
+                },
+            }}>
+            <Stack.Screen
+                name="medication"
+                component={Medication}
+                options={() => {
+                    return {
+                        headerLeft: () => (
+                            <TouchableOpacity style={{ marginRight: 20 }}>
+                                <Feather
+                                    name="menu"
+                                    size={24}
+                                    backgroundColor="#ccfd"
+                                />
+                            </TouchableOpacity>
+                        ),
+                    };
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+function CalendarScreenNavigator() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 22,
+                },
+            }}>
+            <Stack.Screen
+                name="calendar"
+                component={Calendar}
+                options={() => {
+                    return {
+                        headerLeft: () => (
+                            <TouchableOpacity style={{ marginRight: 20 }}>
+                                <Feather
+                                    name="menu"
+                                    size={24}
+                                    backgroundColor="#ccfd"
+                                />
+                            </TouchableOpacity>
+                        ),
+                    };
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
 
 function RootNavigator() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            tabBarOptions={{
+                inactiveTintColor: '#ffbd7d',
+                activeTintColor: '#e47911',
+            }}
+            screenOptions={{
+                headerShown: false,
+            }}>
             <Tab.Screen
                 name="dashboard"
-                component={Dashboard}
+                component={DashboardScreenNavigator}
                 options={{
-                    title: 'dashboard',
                     tabBarIcon: ({ focused, size, color }) => (
                         <FontAwesome name="home" size={30} color={color} />
                     ),
                 }}
             />
             <Tab.Screen
-                name="calendar"
-                component={Calendar}
+                name="medication"
+                component={MedicationScreenNavigator}
                 options={{
                     tabBarIcon: ({ focused, size, color }) => (
                         <FontAwesome5
@@ -45,8 +137,8 @@ function RootNavigator() {
                 }}
             />
             <Tab.Screen
-                name="medication"
-                component={Medication}
+                name="calendar"
+                component={CalendarScreenNavigator}
                 options={{
                     tabBarIcon: ({ focused, size, color }) => (
                         <AntDesign name="calendar" size={size} color={color} />
@@ -54,5 +146,13 @@ function RootNavigator() {
                 }}
             />
         </Tab.Navigator>
+    );
+}
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <RootNavigator />
+        </NavigationContainer>
     );
 }
